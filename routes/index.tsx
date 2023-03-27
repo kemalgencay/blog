794358ecefa5fbd715c18e178ac27f7e9c98305c
kemalgencay <kemalgencay@gmail.com>
2023-03-27 13:16:36 +0300

    first commit

---
+++ routes/index.tsx
@@ -0,0 +1,42 @@
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { listPosts, Post } from "../utils/posts.ts";
+import { State } from "../utils/state.ts";
+
+interface Data extends State {
+  posts: Post[];
+}
+
+export const handler: Handlers<Data, State> = {
+  async GET(_req, ctx) {
+    const posts = await listPosts();
+    return ctx.render({ ...ctx.state, posts });
+  },
+};
+
+export default function Home(props: PageProps<Data>) {
+  const { posts, locales } = props.data;
+  return (
+    <div class="px-4 mx-auto max-w-screen-md">
+      <h1 class="font-bold text-5xl mt-12">Luca's Blog</h1>
+      <ul class="mt-8">
+        {posts.map((post) => <PostEntry post={post} locales={locales} />)}
+      </ul>
+    </div>
+  );
+}
+
+function PostEntry(props: { post: Post; locales: string[] }) {
+  const { post, locales } = props;
+  const dateFmt = new Intl.DateTimeFormat(locales, { dateStyle: "short" });
+  return (
+    <li class="border-t">
+      <a href={`/blog/${post.id}`} class="py-2 flex group gap-4">
+        <div>{dateFmt.format(post.publishAt)}</div>
+        <div>
+          <h2 class="font-bold group-hover:underline">{post.title}</h2>
+          <p class="text-gray-600">{post.snippet}</p>
+        </div>
+      </a>
+    </li>
+  );
+}
---
+++ routes/settings.tsx
@@ -0,0 +1,35 @@
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { setCookie } from "$std/http/cookie.ts";
+import LocaleSelector from "../islands/LocaleSelector.tsx";
+import { State } from "../utils/state.ts";
+
+export const handler: Handlers<State, State> = {
+  GET(_req, ctx) {
+    return ctx.render({ ...ctx.state });
+  },
+  async POST(req) {
+    const form = await req.formData();
+    const locale = form.get("locale");
+    const resp = new Response("", {
+      status: 303,
+      headers: { Location: "/settings" },
+    });
+    if (typeof locale === "string") {
+      setCookie(resp.headers, { name: "locale", value: locale });
+    }
+    return resp;
+  },
+};
+
+export default function SettingsPage(props: PageProps<State>) {
+  const { locales } = props.data;
+  return (
+    <div class="px-4 mx-auto max-w-screen-md">
+      <h1 class="font-bold text-5xl mt-12">Settings</h1>
+      <p class="mt-4">
+        Your current locale is <b>{locales[0]}</b>.
+      </p>
+      <LocaleSelector />
+    </div>
+  );
+}
